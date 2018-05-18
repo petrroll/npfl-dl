1785d0212b033269deadc8fed93cbaaaed0dfd45
Petr Houška <houskape@gmail.com>
2018-05-18 11:16:30 +0200

    Code cleanup.

--- 11/monte_carlo.py
+++ 11/monte_carlo.py
@@ -8,9 +8,55 @@ def try_render_debug(env, args):
     if args.render_each and env.episode and env.episode % args.render_each == 0:
         env.render()
 
+def record_episode(env, args, policy):
+    #Reset environment
+    episode = []
+    episode_reward = 0
+    state, done = env.reset(), False
+
+    while not done:
+        try_render_debug(env, args)
+
+        # figure out current action and move simulation
+        action = np.random.choice(env.actions, replace=False, p=policy[state,:])
+        next_state, reward, done, _ = env.step(action)
+            
+        # append episode data & set next_state as current state
+        episode.append((state, action, reward))
+        episode_reward += reward
+        state = next_state
+
+    return (episode, episode_reward)
+
+def do_mc_learning(policy, avgReturn, stateActionSeen, eps, env):
+    # precompute certain values
+    best_action_p = 1 - eps + (eps / env.actions)
+    others_actions_p = (eps / env.actions)
+
+    # Learn from the just recorded episode
+    G = 0
+    for (state, action, reward) in reversed(episode):
+
+        # Cumulates reward that these actions will get later in this episode
+        G += reward
+
+        # Compute the average reward for current state & action combination.
+        # .. and update number of elements of elements the avg. is made of.
+        avgReturnCoef = stateActionSeen[state, action]
+        avgReturn[state, action] = ((avgReturn[state, action] * avgReturnCoef) + G) / (avgReturnCoef + 1)
+        stateActionSeen[state, action] += 1
+
+        # find the action with highest average return
+        actWithHighestRew = np.argmax(avgReturn[state,:])
+        for actionIndex in range(env.actions):
+            # update policy -> action with highest return (above) gets _static_ high probability all others low
+            newActionP = best_action_p if actionIndex == actWithHighestRew else others_actions_p        
+            policy[state, actionIndex] = newActionP
+
+            
 if __name__ == "__main__":
     # Fix random seed
-    #np.random.seed(42)
+    np.random.seed(42)
 
     # Parse arguments
     import argparse
@@ -53,60 +99,24 @@ if __name__ == "__main__":
     training = True
     while training:
 
-        #Reset environment
-        episode = []
-        episode_reward = 0
-        state, done = env.reset(), False
-
         # Record one episode
-        while not done:
-            try_render_debug(env, args)
-
-            # figure out current action and move simulation
-            action = np.random.choice(env.actions, 1, replace=False, p=policy[state,:])[0] # np.<..> returns array of results
-            next_state, reward, done, _ = env.step(action)
-            
-            # append episode data & set next_state as current state
-            episode.append((state, action, reward))
-            episode_reward += reward
-            state = next_state
+        episode, episode_reward = record_episode(env, args, policy)
         
         # update custom control variables
         rewards_history.append(episode_reward)
         avg_reward = np.mean(rewards_history[-400:])
         eps = args.epsilon - (avg_reward / target_reward) * (args.epsilon - args.epsilon_final)
 
-        # precompute certain values
-        best_action_p = 1 - eps + (eps / env.actions)
-        others_actions_p = (eps / env.actions)
-
         if (avg_reward > treshold_reward):
             training = False
 
-        # Learn from the just recorded episode
-        G = 0
-        for (state, action, reward) in reversed(episode):
+        do_mc_learning(policy, avgReturn, stateActionSeen, eps, env)
 
-            # Cumulates reward that these actions will get later in this episode
-            G += reward
-
-            # Compute the average reward for current state & action combination.
-            # .. and update number of elements of elements the avg. is made of.
-            avgReturnCoef = stateActionSeen[state, action]
-            avgReturn[state, action] = ((avgReturn[state, action] * avgReturnCoef) + G) / (avgReturnCoef + 1)
-            stateActionSeen[state, action] += 1
-
-            # find the action with highest average return
-            actWithHighestRew = np.argmax(avgReturn[state,:])
-            for actionIndex in range(env.actions):
-                # update policy -> action with highest return (above) gets _static_ high probability all others low
-                newActionP = best_action_p if actionIndex == actWithHighestRew else others_actions_p        
-                policy[state, actionIndex] = newActionP
 
     # Perform last 100 evaluation episodes
     for i in range(100):
         state, done = env.reset(start_evaluate=True), False
         while not done:
-            action = np.random.choice(env.actions, 1, replace=False, p=policy[state,:])[0] # np.<..> returns array of results
+            action = np.random.choice(env.actions, replace=False, p=policy[state,:])
             next_state, reward, done, _ = env.step(action)
             state = next_state
